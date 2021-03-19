668dd643f76105451baa1fcbfa6b5d4cde83e7b2
snapper <zihanjashim2121@gmail.comm>
2021-03-20 01:20:48 +0600

    react auth

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -23,8 +23,8 @@ const Destination = () => {
                 <br/>
             </Form>
             </div>
-            <div className="map-img">
-                <img src={mapImg} alt=""/>
+            <div className="card">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233668.3875992415!2d90.27889351177757!3d23.780572945132324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1616181167586!5m2!1sen!2sbd" ></iframe>
             </div>
             </div>
         </div>
